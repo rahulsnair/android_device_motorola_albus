657de129dc983fc6395b60b2621ba3c63d8ce942
rahulsnair <rahulsnair30@gmail.com>
2018-08-19 12:38:06 +0000

    albus : let's not build android.hardware.radio
    Change-Id: I3f5b9a8ec3b2b611f3901c3c0b49315b29807aef

--- device.mk
+++ device.mk
@@ -394,10 +394,6 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     libxml2
 
-#RIL
-PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
