2a2ee5cc79435b501c2b8b86cee485cc4d21fe97
rahulsnair <rahulsnair30@gmail.com>
2018-08-19 12:28:43 +0000

    albus : remove widwine target
     lineage doesn't build this
    
    Change-Id: I20088f5641cd7fbfdd16b6b0eabbc133c268f6ac

--- device.mk
+++ device.mk
@@ -265,9 +265,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service.widevine
+    android.hardware.drm@1.0-service
 
 #PRODUCT_COPY_FILES += \
 #    $(LOCAL_PATH)/configs/qdcm_calib_data_mipi_mot_cmd_smd_1080p_549_alb.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_mipi_mot_cmd_smd_1080p_549_alb.xml
