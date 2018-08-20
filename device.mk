7604257b0db3b4e885facfac1432c43c38ad3b68
rahulsnair <rahulsnair30@gmail.com>
2018-08-20 10:09:03 +0000

    albus : move qualcomm perms to vendor
    Change-Id: I8a28b7c4a7aa984774e88798a0ee027fcf726146

--- device.mk
+++ device.mk
@@ -351,8 +351,8 @@ PRODUCT_PACKAGES += \
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/qti_whitelist.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
