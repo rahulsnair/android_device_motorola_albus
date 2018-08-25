5e35a34feb41aa6ea634e6f3e78e8244a2b9b5a0
rahulsnair <rahulsnair30@gmail.com>
2018-08-25 14:29:42 +0000

    albus : update camera targets and blobs
    Change-Id: Ic69ba0bfbd9479be1781fc59abd5cdfcc7820f9f

--- device.mk
+++ device.mk
@@ -113,8 +113,14 @@ PRODUCT_PACKAGES += \
     Gello
 
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@1.0 \
+    android.hardware.camera.device@3.2 \
+    android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     libbson.vendor \
     libxml2 \
