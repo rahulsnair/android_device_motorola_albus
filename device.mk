1cbb960608301224dcc58d8614d57cde9600df07
rahulsnair <rahulsnair30@gmail.com>
2018-10-07 18:17:18 +0000

    albus : update camera blobs
    Change-Id: I0088ae6b831c2809d27e128092b84c686017cc6b

--- device.mk
+++ device.mk
@@ -127,7 +127,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.common@1.0 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     libbson.vendor \
